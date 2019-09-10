82f9563bfd8aa51ef0445cd7530333e4df384386
ytbdmhy <568870206@qq.com>
2019-09-10 11:49:12 +0800

    1

---
+++ src/main/java/com/ytbdmhy/utils/DateTimeUtil.java
@@ -0,0 +1,33 @@
+package com.ytbdmhy.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTimeUtil {
+
+    public static String getTodayYear() {
+        return getDateYear(new Date());
+    }
+
+    public static String getDateYear(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(date).substring(0, 4);
+    }
+
+    public static String getThisMonthStartDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return dateFormat.format(calendar.getTime());
+    }
+
+    public static String getThisMonthEndDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 1);
+        calendar.set(Calendar.DAY_OF_MONTH, 0);
+        return dateFormat.format(calendar.getTime());
+    }
+}
--- src/main/java/com/ytbdmhy/utils/Test.java
+++ src/main/java/com/ytbdmhy/utils/Test.java
@@ -1,20 +1,9 @@
 package com.ytbdmhy.utils;
 
-import org.springframework.util.StringUtils;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.ytbdmhy.utils.DateStringValidateUtil.getDateString;
-
 public class Test {
 
     public static void main(String[] args) {
-        String dateString = "1990527";
-        System.out.println(getDateString(dateString));
+        String test = "001";
+        System.out.println(Integer.valueOf(test));
     }
 }
