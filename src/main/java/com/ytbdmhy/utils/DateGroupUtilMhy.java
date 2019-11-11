83410242a6dccf73fff4c45cc9af820d7eddb627
ytbdmhy <568870206@qq.com>
2019-11-11 16:13:27 +0800

    111

--- src/main/java/com/ytbdmhy/utils/DateGroupUtilMhy.java
+++ src/main/java/com/ytbdmhy/utils/DateGroupUtilMhy.java
@@ -12,13 +12,13 @@ public class DateGroupUtilMhy {
 
     public static List<String[]> getWeekGroup(String start, String end) {
         try {
+            SimpleDateFormat yMdFormat = new SimpleDateFormat("yyyy-MM-dd");
             int count = start.length() - start.replaceAll("-", "").length();
             if (count == 2 || start.length() > 7) {
-                SimpleDateFormat yMdFormat = new SimpleDateFormat("yyyy-MM-dd");
-                return getWeekGroup(yMdFormat.parse(start), yMdFormat.parse(end), 2);
+                return getWeekGroup(yMdFormat.parse(start), yMdFormat.parse(end));
             } else if (count == 1 || start.length() < 6) {
-                SimpleDateFormat MdFormat = new SimpleDateFormat("MM-dd");
-                return getWeekGroup(MdFormat.parse(start), MdFormat.parse(end), 1);
+                String nowYear = yMdFormat.format(new Date()).substring(0, 5);
+                return getWeekGroup(yMdFormat.parse(nowYear + start), yMdFormat.parse(nowYear + end));
             }
         } catch (ParseException e) {
             e.printStackTrace();
@@ -26,14 +26,8 @@ public class DateGroupUtilMhy {
         return null;
     }
 
-    public static List<String[]> getWeekGroup(Date start, Date end, int mark) {
-        SimpleDateFormat dateFormat;
-        if (mark == 1) {
-            dateFormat = new SimpleDateFormat("MM-dd");
-        } else {
-            dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        }
-
+    public static List<String[]> getWeekGroup(Date start, Date end) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         long days = (end.getTime() - start.getTime()) / 3600 / 24 / 1000;
 
         Calendar instance = Calendar.getInstance();
@@ -42,38 +36,34 @@ public class DateGroupUtilMhy {
         String[] ne;
         int fd = instance.get(Calendar.DAY_OF_WEEK);
         int fwrd;
-        Long adrd = null;
+        long adrd;
         long weeks;
 
-        if (mark == 2) {
-            if (days > 0) {
-                fwrd = fd == 1 ? 0 : 7 - fd + 1;
-                adrd = days - fwrd;
-                weeks = adrd / 7;
-                if (fwrd == 0) {
-                    list.add(new String[]{dateFormat.format(start)});
-                } else {
-                    instance.add(Calendar.DAY_OF_YEAR, fwrd);
-                    list.add(new String[]{dateFormat.format(start), dateFormat.format(instance.getTime())});
-                }
-            } else if (days < 0) {
-                fwrd = fd == 1 ? 6 : fd - 2;
-                adrd = days + fwrd;
-                weeks = adrd / 7;
-                if (fwrd == 0) {
-                    list.add(new String[]{dateFormat.format(start)});
-                } else {
-                    instance.add(Calendar.DAY_OF_YEAR, -fwrd);
-                    list.add(new String[]{dateFormat.format(start), dateFormat.format(instance.getTime())});
-                }
-            } else {
+        if (days > 0) {
+            fwrd = fd == 1 ? 0 : 7 - fd + 1;
+            adrd = days - fwrd;
+            weeks = adrd / 7;
+            if (fwrd == 0) {
                 list.add(new String[]{dateFormat.format(start)});
-                return list;
+            } else {
+                instance.add(Calendar.DAY_OF_YEAR, fwrd);
+                list.add(new String[]{dateFormat.format(start), dateFormat.format(instance.getTime())});
             }
-            instance.add(Calendar.DAY_OF_YEAR, adrd > 0 ? 1 : -1);
+        } else if (days < 0) {
+            fwrd = fd == 1 ? 6 : fd - 2;
+            adrd = days + fwrd;
+            weeks = adrd / 7;
+            if (fwrd == 0) {
+                list.add(new String[]{dateFormat.format(start)});
+            } else {
+                instance.add(Calendar.DAY_OF_YEAR, -fwrd);
+                list.add(new String[]{dateFormat.format(start), dateFormat.format(instance.getTime())});
+            }
         } else {
-            weeks = days / 7;
+            list.add(new String[]{dateFormat.format(start)});
+            return list;
         }
+        instance.add(Calendar.DAY_OF_YEAR, adrd > 0 ? 1 : -1);
 
         if (weeks > 0) {
             for (int i = 0;i < weeks;i++) {
@@ -95,31 +85,29 @@ public class DateGroupUtilMhy {
             }
         }
 
-        if (mark == 2) {
-            if (Math.abs(adrd % 7) == 1) {
-                list.add(new String[]{dateFormat.format(end)});
-            } else if (Math.abs(adrd % 7) > 0) {
-                list.add(new String[]{dateFormat.format(instance.getTime()), dateFormat.format(end)});
-            }
-        } else {
-            if (Math.abs(days % 7) == 1) {
-                list.add(new String[]{dateFormat.format(end)});
-            } else if (Math.abs(days % 7) > 0) {
-                list.add(new String[]{dateFormat.format(instance.getTime()), dateFormat.format(end)});
-            }
+        if (Math.abs(adrd % 7) == 1) {
+            list.add(new String[]{dateFormat.format(end)});
+        } else if (Math.abs(adrd % 7) > 0) {
+            list.add(new String[]{dateFormat.format(instance.getTime()), dateFormat.format(end)});
         }
         return list;
     }
 
     public static void main(String[] args) {
-        String start = "2019-08-19";
-        String end = "2019-05-26";
+        String start = "08-22";
+        String end = "05-26";
         for (String[] week : getWeekGroup(start, end)) {
             System.out.println(Arrays.toString(week));
         }
+        long t1 = System.currentTimeMillis();
         System.out.println("------------------------------");
         for (String[] week : getWeekGroup(end, start)) {
             System.out.println(Arrays.toString(week));
         }
+        System.out.println(System.currentTimeMillis() - t1);
+
+        // 以下参数,数组最后一位有问题
+//        String start = "08-19";
+//        String end = "05-26";
     }
 }
